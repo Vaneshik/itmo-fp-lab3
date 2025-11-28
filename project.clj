ae5959fd36c731a6f6810d42183d7afac066570e
Vaneshik <makschus05@mail.ru>
2025-11-28 16:23:47 +0300

    init: project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fp-lab-3 "1.0"
+  :description "Stream data interpolation"
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/core.async "1.6.681"]]
+  :main itmo-fp-lab3.core)
