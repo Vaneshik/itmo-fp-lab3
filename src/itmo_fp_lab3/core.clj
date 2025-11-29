b94531cd550441256016a33c7107a5b5312b7c9f
Vaneshik <makschus05@mail.ru>
2025-11-29 18:41:52 +0300

    feat: base project structure

---
+++ src/itmo_fp_lab3/core.clj
@@ -0,0 +1,7 @@
+(ns itmo-fp-lab3.core
+  (:require
+   [clojure.tools.cli :refer [parse-opts]]
+   [clojure.core.async :as async]))
+
+(defn -main [& args]
+  (println "Starting interpolation service..."))
